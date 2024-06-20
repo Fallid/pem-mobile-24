58f358aa4052e67262031f3658d788431b119155
Ahmad Naufal <naufalopal002@gmail.com>
2024-06-20 19:07:35 +0700

    fix project ALL DONE

--- modul_5 copy/android/app/src/main/kotlin/com/dipo/modul_2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dipo.modul_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- modul_5 copy/lib/app/modules/news/bindings/news_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:modul_2/app/modules/news/controllers/news_controller.dart';
-
-class NewsBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => NewsController());
-  }
-}
